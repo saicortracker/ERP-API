71a186ad08e4ec1f93e6dc7ba5a06880955bdead
cuddapahlakshmi <lakshmi@cortracker.com>
2024-05-24 16:19:01 +0530

    update

--- CrmContacts/obj/Debug/net6.0/ERP.AssemblyInfo.cs
+++ CrmContacts/obj/Debug/net6.0/ERP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a09ddc5049d848d15cc443381a2d558f83c3e99")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
